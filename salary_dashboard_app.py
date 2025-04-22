69f46271051277ce74f8a519fbac8be247d12682
ruth2126 <ruthoyinlola@yahoo.co.uk>
2025-04-22 21:40:23 +0100

    loading of dataset completed

--- salary_dashboard_app.py
+++ salary_dashboard_app.py
@@ -1,1 +1,10 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+df = pd.read_csv("/Users/Ruthf/Downloads/w1920503/project_lifecycle_coursework/Average_Salary_by_Job_Classification.csv")
+
+st.write("salary dashboard application")
+st.dataframe(df.head())
+st.dataframe(df.shape)
